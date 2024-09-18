a9accdad836c270baee0a3506850c98fd30f2b06
Leyton Cespedes <leytoncespedes123@gmail.com>
2024-09-18 17:01:18 -0400

    Funcion suma Leyton Cespedes

---
+++ suma.py
@@ -0,0 +1,5 @@
+# Realisado por Leyton cespedes
+def sumar():
+    numero1 = int(input("ingrese el 1er numero a sumar: "))
+    numero2 = int(input("ingrese el 2do numero a sumar : "))
+    return numero1 + numero2
